1b5e98f4bcd52026fb4c788e7bba5c9433890828
Khushal020 <khushalgondaliya020@gmail.com>
2020-06-13 19:26:20 +0530

    Contour for first form

--- VisionAPI/VisionModule/ByContour.py
+++ VisionAPI/VisionModule/ByContour.py
@@ -7,7 +7,8 @@ MASK_DILATE_ITER = 10
 MASK_ERODE_ITER = 10
 MASK_COLOR = (0.0,0.0,0.0) # In BGR format\
 
-img = cv2.imread('Original/removedLine.jpg')
+img = cv2.imread('Original/sampleform-1.jpg')
+img = cv2.resize(img, (780, 540), interpolation = cv2.INTER_NEAREST) 
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 #gray1 = gray.copy()
 
@@ -30,18 +31,23 @@ contour_info = sorted(contour_info, key=lambda c: c[2], reverse=True)
 
 #max_contour = contour_info[0]
 #(x, y, w, h) = cv2.boundingRect(max_contour[0])
+#cv2.rectangle(img, (x,y), (x+w, y+h), (255, 0, 0), 2)
 k = 0
 for i in contour_info:
     (x, y, w, h) = cv2.boundingRect(i[0])
     cv2.rectangle(img, (x,y), (x+w, y+h), (255, 0, 0), 2)
     
-    #cv2.imshow('image', img)
-    #cv2.waitKey(0)
-    #cv2.destroyAllWindows()
+    print(i[2])
+    
+    cv2.imshow('image', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
     
     k += 1
-    if k > 37:
+    if k > 15:
         break
+    
+print(k)
 
 #cv2.rectangle(img, (x,y), (x+w, y+h), (255, 0, 0), 2)
 
