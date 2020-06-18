01200438e0885a7d6484bf42095a729ecd7a8521
keyurkhant <kpkhant007@yahoo.com>
2020-06-18 10:03:26 +0530

    Changes by KP

--- VisionAPI/ImageROI&Text/FileOps.py
+++ VisionAPI/ImageROI&Text/FileOps.py
@@ -1,2 +1,9 @@
 file = open('text8.txt', 'r')
 lines = file.readlines()
+
+# Remove all content before first line of form
+# This is required because there are many noice in image which are recognized as character by OCR.
+firstIndex = lines.index('Cologuard Order Number:\n')
+
+lines = lines[firstIndex:]
+
---
+++ VisionAPI/VisionModule/ByContour.py
@@ -0,0 +1,61 @@
+import cv2
+
+#img = cv2.imread('Original/sampleform.png')
+
+BLUR = 21
+CANNY_THRESH_1 = 10
+CANNY_THRESH_2 = 200
+MASK_DILATE_ITER = 10
+MASK_ERODE_ITER = 10
+MASK_COLOR = (0.0,0.0,0.0) # In BGR format\
+
+img = cv2.imread('/root/Keyur Khant/Study/Others/OCR Hackathon/Original/sampleform-2.jpg')
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+#gray1 = gray.copy()
+
+#_, th1 = cv2.threshold(gray,198, 255, cv2.THRESH_BINARY)
+
+edges = cv2.Canny(gray,CANNY_THRESH_1, CANNY_THRESH_2)
+edges = cv2.dilate(edges, None)
+edges = cv2.erode(edges, None)
+
+#cv2.imshow('image1', edges)
+
+contour_info = []
+contours, hierarchy = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+for c in contours:
+    contour_info.append((c, cv2.isContourConvex(c), cv2.contourArea(c),))
+contour_info = sorted(contour_info, key=lambda c: c[2], reverse=True)
+
+#max_contour = contour_info[0]
+#(x, y, w, h) = cv2.boundingRect(max_contour[0])
+
+file = open('/root/Keyur Khant/Study/Others/OCR Hackathon/VisionAPI/VisionModule/result/resulttext.txt' , 'a')
+file.write('X,W,Y,H\n')
+
+k = 0
+count = 1
+for i in contour_info:
+    (x, y, w, h) = cv2.boundingRect(i[0])
+    cv2.rectangle(img, (x,y), (x+w, y+h), (255, 0, 0), 2)
+    txt = str(x)+","+str(w)+","+str(y)+","+str(h)+"\n"
+    file.write(txt)
+    #cv2.imwrite("/root/Keyur Khant/Study/Others/OCR Hackathon/VisionAPI/VisionModule/result/img"+str(count)+'.jpg' , img[y:y+h ,x:x+w])
+    #count += 1
+    #cv2.imshow('image', img)
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()
+    
+    k += 1
+    if k > 37:
+        break
+
+#cv2.imshow('123' , img[y:y+h ,x:x+w])
+
+#cv2.rectangle(img, (x,y), (x+w, y+h), (255, 255, 255), 2)
+file.close()
+print("Done")
+#cv2.imwrite('result1.jpg', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
