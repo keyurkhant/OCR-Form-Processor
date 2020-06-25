77de39e2b6f91c2cac6fd303d84c6142163ff7f0
keyurkhant <kpkhant007@yahoo.com>
2020-06-25 14:35:47 +0530

    ROI final

--- VisionAPI/Extra/ROIGeneration.py
+++ VisionAPI/Extra/ROIGeneration.py
@@ -6,36 +6,38 @@ def getROI():
 	
 	img = cv.imread('/root/Keyur Khant/Study/Others/OCR Hackathon/VisionAPI/VisionModule/result4/img1.jpg')
 	
+	#standard Pixel for this
 	img = cv.resize(img , (1443 , 1702))
 
+	cv.imwrite('output.jpg' , img)
 	imgList = []
 
 	# HCO Name
-	imgCrop1 = img[100:172,0:720]
+	imgCrop1 = img[100:172,368:718]
 	imgList.append(imgCrop1)
 
 	# Provider Name
-	imgCrop2 = img[160:235,0:720] 
+	imgCrop2 = img[160:233,190:718] 
 	imgList.append(imgCrop2)	
 
 	# NPI Number
-	imgCrop3 = img[230:320,0:720]
+	imgCrop3 = img[226:320,80:718]
 	imgList.append(imgCrop3)
 
 	# Provider Address 
-	imgCrop4 = img[310:380,0:720]
+	imgCrop4 = img[310:380,215:718]
 	imgList.append(imgCrop4)
 
 	# Provider City
-	imgCrop5 = img[370:440,0:720]
+	imgCrop5 = img[370:440,180:718]
 	imgList.append(imgCrop5)
 
 	# Provider Phone
-	imgCrop6 = img[430:500,0:720]
+	imgCrop6 = img[430:500,195:718]
 	imgList.append(imgCrop6)
 
 	# Provider Fax
-	imgCrop7 = img[490:560,0:720]
+	imgCrop7 = img[490:560,255:718]
 	imgList.append(imgCrop7)
 
 
@@ -43,125 +45,121 @@ def getROI():
 
 #################################
 
-	# ICD 10 code
-	imgCrop3 = img[210:360,720:1443]
+	# ICD 10 code (if Others then)
+	imgCrop3 = img[297:360,855:1420]
 	imgList.append(imgCrop3)
 
 	# Date of order 
-	imgCrop4 = img[500:600,1110:1443]
+	imgCrop4 = img[513:562,1110:1420]
 	imgList.append(imgCrop4)
 
 #################################
 
 	# Patient ID
-	imgCrop5 = img[640:700,0:720]
+	imgCrop5 = img[650:710,200:718]
 	imgList.append(imgCrop5)
 
 	# Patient first name
-	imgCrop6 = img[695:750,0:315]
+	imgCrop6 = img[695:750,140:313]
 	imgList.append(imgCrop6)
 
 	# Patient Last Name
-	imgCrop7 = img[695:750,312:720]
+	imgCrop7 = img[695:750,442:718]
 	imgList.append(imgCrop7)
 
 	# Patient DOB
-	imgCrop5 = img[745:805,0:420]
+	imgCrop5 = img[745:795,235:420]
 	imgList.append(imgCrop5)
 
 	# Patient sex
-	imgCrop6 = img[745:805,415:720]
+	imgCrop6 = img[745:795,467:720]
 	imgList.append(imgCrop6)
 
 	# Patient Phone No
-	imgCrop7 = img[640:720,715:1443]
+	imgCrop7 = img[650:720,1045:1420]
 	imgList.append(imgCrop7)
 
 	# Patient language
-	imgCrop5 = img[715:785,715:1443]
+	imgCrop5 = img[715:785,1115:1420]
 	imgList.append(imgCrop5)
 
 	# Patient shipping address
-	imgCrop6 = img[795:895,0:720]
+	imgCrop6 = img[800:910,220:718]
 	imgList.append(imgCrop6)
 
 	# Patient shipping city
-	imgCrop7 = img[885:955,0:720]
+	imgCrop7 = img[895:950,195:718]
 	imgList.append(imgCrop7)
 
 	# Patient billing address
-	imgCrop6 = img[795:895,715:1433]
+	imgCrop6 = img[795:910,925:1420]
 	imgList.append(imgCrop6)
 
-	# Patient shipping city
-	imgCrop7 = img[885:955,715:1443]
+	# Patient billing city
+	imgCrop7 = img[895:950,915:1420]
 	imgList.append(imgCrop7)
 
 
 #####################################
 
 	# Patient latino
-	imgCrop6 = img[1000:1050,0:1443]
+	imgCrop6 = img[1000:1050,640:840]
 	imgList.append(imgCrop6)
 
 	# Patient race
-	imgCrop7 = img[1050:1120,0:1443]
+	imgCrop7 = img[1075:1118,10:1420]
 	imgList.append(imgCrop7)	
 
 
 ####################################
 	
 	# parient has insurance?
-	imgCrop5 = img[1180:1235,0:1443]
+	imgCrop5 = img[1190:1235,660:1420]
 	imgList.append(imgCrop5)
 
 	# policy holder name
-	imgCrop6 = img[1220:1275,0:475]
+	imgCrop6 = img[1225:1285,236:475]
 	imgList.append(imgCrop6)
 
 	# policy holder dob
-	imgCrop7 = img[1220:1275,475:835]
+	imgCrop7 = img[1225:1285,685:825]
 	imgList.append(imgCrop7)
 
 	# Relation
-	imgCrop6 = img[1220:1275,830:1443]
+	imgCrop6 = img[1225:1285,1105:1425]
 	imgList.append(imgCrop6)
 
 	# insurance carrier
-	imgCrop7 = img[1275:1330,0:605]
+	imgCrop7 = img[1270:1340,315:605]
 	imgList.append(imgCrop7)
 
 	# insurance type
-	imgCrop7 = img[1275:1330,600:1443]
+	imgCrop7 = img[1270:1340,666:1430]
 	imgList.append(imgCrop7)	
 
 	# insurance address
-	imgCrop6 = img[1320:1375,0:1443]
+	imgCrop6 = img[1325:1385,333:1430]
 	imgList.append(imgCrop6)
 
 	# policy number
-	imgCrop7 = img[1370:1430,0:570]
+	imgCrop7 = img[1370:1440,350:565]
 	imgList.append(imgCrop7)
 
 	# Group
-	imgCrop7 = img[1370:1430,570:1000]
+	imgCrop7 = img[1370:1440,745:1005]
 	imgList.append(imgCrop7)
 
 	# Plan
-	imgCrop7 = img[1370:1430,1000:1443]
+	imgCrop7 = img[1370:1440,1065:1430]
 	imgList.append(imgCrop7)
 
 	# Autho code
-	imgCrop7 = img[1425:1485,0:1443]
+	imgCrop7 = img[1420:1475,450:1400]
 	imgList.append(imgCrop7)
 
 	# Final Date
-	imgCrop7 = img[1640:1702,950:1443]
+	imgCrop7 = img[1640:1687,1020:1343]
 	imgList.append(imgCrop7)	
 	
-	#for img in imgList:
-	#	cv.imshow('Window', img)
-	#	cv.waitKey(0)
-	#	cv.destroyAllWindows()
-
 	return imgList
+
--- VisionAPI/VisionModule/ROIGeneration.py
+++ VisionAPI/VisionModule/ROIGeneration.py
@@ -4,36 +4,38 @@ from pdf2image import convert_from_bytes
 
 def getROI(img):
 	
+	#standard Pixel for this
 	img = cv.resize(img , (1443 , 1702))
 
+	cv.imwrite('output.jpg' , img)
 	imgList = []
 
 	# HCO Name
-	imgCrop1 = img[100:172,0:720]
+	imgCrop1 = img[100:172,380:718]
 	imgList.append(imgCrop1)
 
 	# Provider Name
-	imgCrop2 = img[160:235,0:720] 
+	imgCrop2 = img[160:233,198:718] 
 	imgList.append(imgCrop2)	
 
 	# NPI Number
-	imgCrop3 = img[230:320,0:720]
+	imgCrop3 = img[226:320,88:718]
 	imgList.append(imgCrop3)
 
 	# Provider Address 
-	imgCrop4 = img[310:380,0:720]
+	imgCrop4 = img[310:380,223:718]
 	imgList.append(imgCrop4)
 
 	# Provider City
-	imgCrop5 = img[370:440,0:720]
+	imgCrop5 = img[370:440,188:718]
 	imgList.append(imgCrop5)
 
 	# Provider Phone
-	imgCrop6 = img[430:500,0:720]
+	imgCrop6 = img[430:500,203:718]
 	imgList.append(imgCrop6)
 
 	# Provider Fax
-	imgCrop7 = img[490:560,0:720]
+	imgCrop7 = img[490:560,263:718]
 	imgList.append(imgCrop7)
 
 
@@ -41,125 +43,121 @@ def getROI(img):
 
 #################################
 
-	# ICD 10 code
-	imgCrop3 = img[210:360,720:1443]
+	# ICD 10 code (if Others then)
+	imgCrop3 = img[297:360,855:1420]
 	imgList.append(imgCrop3)
 
 	# Date of order 
-	imgCrop4 = img[500:600,1110:1443]
+	imgCrop4 = img[513:562,1110:1420]
 	imgList.append(imgCrop4)
 
 #################################
 
 	# Patient ID
-	imgCrop5 = img[640:700,0:720]
+	imgCrop5 = img[650:710,205:718]
 	imgList.append(imgCrop5)
 
 	# Patient first name
-	imgCrop6 = img[695:750,0:315]
+	imgCrop6 = img[695:750,148:313]
 	imgList.append(imgCrop6)
 
 	# Patient Last Name
-	imgCrop7 = img[695:750,312:720]
+	imgCrop7 = img[695:750,450:718]
 	imgList.append(imgCrop7)
 
 	# Patient DOB
-	imgCrop5 = img[745:805,0:420]
+	imgCrop5 = img[745:795,243:425]
 	imgList.append(imgCrop5)
 
 	# Patient sex
-	imgCrop6 = img[745:805,415:720]
+	imgCrop6 = img[745:795,475:720]
 	imgList.append(imgCrop6)
 
 	# Patient Phone No
-	imgCrop7 = img[640:720,715:1443]
+	imgCrop7 = img[650:720,1053:1420]
 	imgList.append(imgCrop7)
 
 	# Patient language
-	imgCrop5 = img[715:785,715:1443]
+	imgCrop5 = img[715:785,1123:1420]
 	imgList.append(imgCrop5)
 
 	# Patient shipping address
-	imgCrop6 = img[795:895,0:720]
+	imgCrop6 = img[800:910,228:718]
 	imgList.append(imgCrop6)
 
 	# Patient shipping city
-	imgCrop7 = img[885:955,0:720]
+	imgCrop7 = img[895:950,203:718]
 	imgList.append(imgCrop7)
 
 	# Patient billing address
-	imgCrop6 = img[795:895,715:1433]
+	imgCrop6 = img[795:910,933:1420]
 	imgList.append(imgCrop6)
 
-	# Patient shipping city
-	imgCrop7 = img[885:955,715:1443]
+	# Patient billing city
+	imgCrop7 = img[895:950,923:1420]
 	imgList.append(imgCrop7)
 
 
 #####################################
 
 	# Patient latino
-	imgCrop6 = img[1000:1050,0:1443]
+	imgCrop6 = img[1000:1050,640:840]
 	imgList.append(imgCrop6)
 
 	# Patient race
-	imgCrop7 = img[1050:1120,0:1443]
+	imgCrop7 = img[1075:1118,10:1420]
 	imgList.append(imgCrop7)	
 
 
 ####################################
 	
 	# parient has insurance?
-	imgCrop5 = img[1180:1235,0:1443]
+	imgCrop5 = img[1190:1235,668:1420]
 	imgList.append(imgCrop5)
 
 	# policy holder name
-	imgCrop6 = img[1220:1275,0:475]
+	imgCrop6 = img[1225:1285,242:475]
 	imgList.append(imgCrop6)
 
 	# policy holder dob
-	imgCrop7 = img[1220:1275,475:835]
+	imgCrop7 = img[1225:1285,685:825]
 	imgList.append(imgCrop7)
 
 	# Relation
-	imgCrop6 = img[1220:1275,830:1443]
+	imgCrop6 = img[1225:1285,1105:1425]
 	imgList.append(imgCrop6)
 
 	# insurance carrier
-	imgCrop7 = img[1275:1330,0:605]
+	imgCrop7 = img[1270:1340,318:605]
 	imgList.append(imgCrop7)
 
 	# insurance type
-	imgCrop7 = img[1275:1330,600:1443]
+	imgCrop7 = img[1270:1340,666:1430]
 	imgList.append(imgCrop7)	
 
 	# insurance address
-	imgCrop6 = img[1320:1375,0:1443]
+	imgCrop6 = img[1325:1385,336:1430]
 	imgList.append(imgCrop6)
 
 	# policy number
-	imgCrop7 = img[1370:1430,0:570]
+	imgCrop7 = img[1370:1440,353:565]
 	imgList.append(imgCrop7)
 
 	# Group
-	imgCrop7 = img[1370:1430,570:1000]
+	imgCrop7 = img[1370:1440,748:1005]
 	imgList.append(imgCrop7)
 
 	# Plan
-	imgCrop7 = img[1370:1430,1000:1443]
+	imgCrop7 = img[1370:1440,1068:1430]
 	imgList.append(imgCrop7)
 
 	# Autho code
-	imgCrop7 = img[1425:1485,0:1443]
+	imgCrop7 = img[1420:1475,453:1400]
 	imgList.append(imgCrop7)
 
 	# Final Date
-	imgCrop7 = img[1640:1702,950:1443]
+	imgCrop7 = img[1640:1687,1020:1343]
 	imgList.append(imgCrop7)	
 	
-	#for img in imgList:
-	#	cv.imshow('Window', img)
-	#	cv.waitKey(0)
-	#	cv.destroyAllWindows()
-
 	return imgList
+
